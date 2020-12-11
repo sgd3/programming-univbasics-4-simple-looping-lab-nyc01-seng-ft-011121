ac1c1ddcacbe19d0b8d45f6c55c41706803552ba
Steve Daniel <sgd3@cornell.edu>
2020-12-11 21:53:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
+5.times do
+  puts "Hello World."
+end
+
+
 # Write your methods here
+
+def loop_message_n_times(message, number)
+  number = 5
+  counter = 0
+  until counter == 5
+    puts "Hello World"
+    counter += 1
+  end
+
+
+def output_array
+  array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+  puts array
+  end
+end
